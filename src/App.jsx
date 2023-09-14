cdde56dddf1e6e39735e29ea0d82f152724fc122
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-09-15 08:45:52 +0900

    Add the json file and ready the environment  to write code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-error">hi</button>
+    </>
+  );
+}
+
+export default App;
